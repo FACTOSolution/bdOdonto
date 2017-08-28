b91762853c21496900a05c4338e25ec97c3815f8
rafamb <rfebarros@gmail.com>
2017-08-28 15:18:57 -0300

    Atualizacao models e separacao do model da ficha de diagnostico em outro arquivo

--- bdOdonto/sistema_fichas/models.py
+++ bdOdonto/sistema_fichas/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.utils import timezone
 
-class aluno (models.Model):
+from ficha_diagnostico import *
+
+class Aluno (models.Model):
 	nome = models.CharField(max_length=50)
 	matricula = models.PositiveIntegerField(primary_key=True)
 	login = models.CharField(max_length=20)
@@ -13,12 +15,12 @@ class aluno (models.Model):
     def __str__(self):
         return self.matricula
 
-class professor (models.Model):
+class Professor (models.Model):
 	nome = models.CharField(max_length=50)
 	code = models.PositiveIntegerField(primary_key=True)
 	login = models.CharField(max_length=20)
 	senha = models.CharField(max_length=20)
-	turmas = models.ManyToManyField(turma)
+	turmas = models.ManyToManyField(Turma)
 
 	def publish(self):
         self.save()
@@ -26,7 +28,7 @@ class professor (models.Model):
     def __str__(self):
         return self.code
 
-class fichas(models.Models):
+class Tipo_Fichas(models.Models):
 	code = models.PositiveIntegerField(primary_key=True)
 	nome = models.CharField(max_length=50)
 
@@ -37,11 +39,11 @@ class fichas(models.Models):
         return self.code
 
 
-class turma (models.Model):
+class Turma (models.Model):
 	code = models.CharField(max_length=7, primary_key=True)
 	nome = models.CharField(max_length=30)
-	ficha = models.ManyToManyField(fichas)
-	alunos = models.ManyToManyField(aluno,through='turma_aluno')
+	fichas = models.ManyToManyField(Tipo_Fichas)
+	alunos = models.ManyToManyField(Aluno,through='Turma_Aluno')
 
 	def publish(self):
         self.save()
@@ -49,9 +51,9 @@ class turma (models.Model):
     def __str__(self):
         return self.code
 
-class turma_aluno (models.Model):
-	turma = models.ForeignKey(turma, on_delete=models.CASCADE)
-	aluno = models.ForeignKey(aluno, on_delete=models.CASCADE)
+class Turma_Aluno (models.Model):
+	turma = models.ForeignKey(Turma, on_delete=models.CASCADE)
+	aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
 	code = models.CharField(max_length=7, primary_key=True)
 	periodo = models.CharField(max_length=6)
 
@@ -63,7 +65,7 @@ class turma_aluno (models.Model):
 
 
 
-class paciente(models.Model):
+class Paciente(models.Model):
 	cpf = models.PositiveIntegerField(primary_key=True)
 	nome = models.CharField(max_length=200)
 	endereco = models.CharField(max_length=200)
@@ -92,7 +94,7 @@ class paciente(models.Model):
 	bairro_profissional = models.CharField(max_length=200,blank = True, null = True)
 	cep_profissional = models.PositiveIntegerField(blank = True, null = True)
 
-	turma_aluno = models.ManyToManyField(turma_aluno, through = "atendimento")
+	turma_Aluno = models.ManyToManyField(Turma_Aluno, through = "atendimento")
 	
 	def publish(self):
         self.save()
@@ -100,132 +102,9 @@ class paciente(models.Model):
     def __str__(self):
         return self.cpf
 	
-class Atendimentos (models.Model):
-	data = models.DateTimeField()
+class Atendimento (models.Model):
+	data = models.DateTimeField(default=timezone.now)
 	code = models.CharField(max_length=7, primary_key=True)
-	tipo_ficha = models.ForeignKey(fichas, on_delete=models.CASCADE)
-	paciente = models.ForeignKey(paciente, on_delete=models.CASCADE)
-	turma_aluno = models.ForeignKey(turma_aluno, on_delete=models.CASCADE)
-
-
-class ficha_diagnostico(models.Model):
-	#aluno = models.ForeignKey('aluno.cpf').
-    #paciente = models.ForeignKey('paciente')
-    numero = models.PositiveIntegerField(primary_key=True)
-    data = models.DateTimeField(default=timezone.now)
-
-    motivo = models.CharField(max_length=200)
-    historia = models.TextField()
-
-    ultima_consulta = models.DateTimeField(blank = True, null = True)
-    frequencia_consultas = models.CharField(max_length=200)
-    higiene_propria = models.CharField(max_length=200)
-    frequencia_escova = models.PositiveIntegerField()
-    dentes_sensiveis = models.BooleanField()
-    sangramento_gengiva = models.BooleanField()
-    morde_objetos = models.BooleanField()
-    mobilidade = models.BooleanField()
-    protese = models.BooleanField()
-    range_dentes = models.BooleanField()
-    dificuldade_abrir = models.BooleanField()
-    estalido = models.BooleanField()
-    boca_seca = models.BooleanField()
-    sol_frequente = models.BooleanField()
-    tabagismo = models.BooleanField()
-    tipo_tabagismo = models.CharField(max_length=200,blank=True,null=True)
-    duracao_tabagismo = models.CharField(max_length=200,blank=True,null=True)
-    frequencia_tabagismo = models.CharField(max_length=200,blank=True,null=True)
-    tempo_abandono_tabagismo = models.CharField(max_length=200,blank=True,null=True)
-    alcool = models.BooleanField()
-    frequencia_alcool = models.CharField(max_length=200,blank=True,null=True)
-    drogas_ilicitas = models.BooleanField()
-    def_drogas_ilicitas = models.CharField(max_length=200,blank=True,null=True)
-    tratamento_medico = models.BooleanField()
-    def_tratamento_medico = models.CharField(max_length=200,blank=True,null=True)
-    medicacao = models.BooleanField()
-    def_medicacao = models.CharField(max_length=200,blank=True,null=True)
-    doenca_grave = models.BooleanField()
-    def_doenca_grave = models.CharField(max_length=200,blank=True,null=True)
-    cirurgia = models.BooleanField()
-    def_cirurgia = models.CharField(max_length=200,blank=True,null=True)
-    anticoncepcional = models.BooleanField()
-    gravida = models.BooleanField()
-    tempo_gravidez = models.CharField(max_length=200,blank=True,null=True)
-    alergia = models.BooleanField()
-    def_alergia = models.CharField(max_length=200,blank=True,null=True)
-    reacao_medicamento = models.BooleanField()
-    def_reacao_medicamento = models.CharField(max_length=200,blank=True,null=True)
-    anestesia_dentaria = models.BooleanField()
-    reacao_anestesia_dentaria = models.CharField(max_length=200,blank=True,null=True)
-    anestesia_geral = models.BooleanField()
-    reacao_anestesia_geral = models.CharField(max_length=200,blank=True,null=True)
-
-    D_RESPS = (
-    	('P','Pneumonia'),
-    	('S','Sinusite'),
-    	('R','Rinite'),
-    	('B','Bronquite'),
-    	('A','Asma'),
-    	('O','Outro'),
-    	)
-    disturbios_respiratorios = models.BooleanField()
-    disturbios_respiratorios_abaixo = models.CharField(max_length=1,blank=True,null=True,choices=D_RESPS)
-    hipertenso = models.BooleanField()
-    pressao_arterial = models.CharField(max_length=200,blank=True,null=True)
-    sangramento_excesso = models.BooleanField()
-    palpitacao = models.BooleanField()
-    falta_ar = models.BooleanField()
-    pes_inchados = models.BooleanField()
-    febre_reumatica = models.BooleanField()
-    problema_cardiovascular = models.BooleanField()
-    def_problema_cardiovascular = models.CharField(max_length=200,blank=True,null=True)
-
-    D_TRANS = (
-    	('He','Hepatite'),
-    	('S','Sifilis'),
-    	('T','Tuberculose'),
-    	('Ha','Hanseniase'),
-    	('A','Aids'),
-    	('O','Outro'),
-    	)
-    doencas_transmissiveis = models.BooleanField()
-    doencas_transmissiveis_abaixo = models.CharField(max_length=2,blank=True,null=True,choices=D_RESPS)
-
-    virus = models.BooleanField()
-    def_virus = models.CharField(max_length=200,blank=True,null=True)
-    diabetes = models.BooleanField()
-    cicatrizacao_demorada = models.BooleanField()
-    perda_peso = models.BooleanField()
-    aumento_freq_urina = models.BooleanField()
-    desmaios = models.BooleanField()
-    convulsoes = models.BooleanField()
-    epilepsia = models.BooleanField()
-    disturbio_sanguineo = models.BooleanField()
-    def_disturbio_sanguineo = models.CharField(max_length=200,blank=True,null=True)
-    outro_problema = models.BooleanField()
-    def_outro_problema = models.CharField(max_length=200,blank=True,null=True)
-
-
-    dentes_sensiveis = models.BooleanField()
-    dentes_sensiveis = models.BooleanField()
-
-    face = models.TextField()
-    atm = models.TextField()
-    m_mastigatorios = models.TextField()
-    g_salivares = models.TextField()
-    g_linfaticos = models.TextField()
-    labios = models.TextField()
-    mucosa_j = models.TextField()
-    soalho_boca = models.TextField()
-    lingua = models.TextField()
-    palato = models.TextField()
-    orofaringe = models.TextField()
-    percussao = models.TextField()
-    exames_complementares = models.TextField()
-
-    
-    def publish(self):
-        self.save()
-
-    def __str__(self):
-        return self.numero
+	tipo_ficha = models.ForeignKey(Tipo_Fichas, on_delete=models.CASCADE)
+	paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
+	turma_Aluno = models.ForeignKey(Turma_Aluno, on_delete=models.CASCADE)
