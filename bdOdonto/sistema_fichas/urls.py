84654e6747c7f89fc792163657bdadadbd830944
Igor Rafael <yggor14rafa@hotmail.com>
2017-09-03 20:08:54 -0300

    Update urls.py

--- bdOdonto/sistema_fichas/urls.py
+++ bdOdonto/sistema_fichas/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^odontograma$', views.odontograma, name='odontograma'),
+    url(r'^odontograma/$', views.odontograma),
 ]
 
