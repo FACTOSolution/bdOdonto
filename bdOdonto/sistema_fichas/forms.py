0657a037148a22de03e14c0931599af18fd44ecb
Igor Rafael <yggor14rafa@hotmail.com>
2017-09-04 10:07:18 -0300

    Adding form AlunosForm

--- bdOdonto/sistema_fichas/forms.py
+++ bdOdonto/sistema_fichas/forms.py
@@ -3,6 +3,12 @@ from django import forms
 
 from .models import *
 
+class AlunoForm(forms.ModelForm):
+
+    class Meta:
+        model = Aluno
+        fields = '__all__'
+
 class Ficha_UrgenciaForm(forms.ModelForm):
 
     class Meta:
